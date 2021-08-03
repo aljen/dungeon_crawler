a6d6757dd469593181125c56d0286408eabe0a77
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2021-08-04 01:02:32 +0200

    Add player & movement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod map;
+mod player;
 
 mod prelude {
     pub use crate::map::*;
+    pub use crate::player::*;
     pub use bracket_lib::prelude::*;
 
     pub const SCREEN_WIDTH: i32 = 80;
@@ -12,11 +14,15 @@ use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
@@ -24,7 +30,9 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
