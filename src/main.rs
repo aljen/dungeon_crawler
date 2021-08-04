5c9ae43bf0f5a93257b3a9f35eeac06ea41970f3
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2021-08-04 19:50:45 +0200

    Move to turn based logic

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod map;
 mod map_builder;
 mod spawner;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use crate::camera::*;
@@ -12,6 +13,7 @@ mod prelude {
     pub use crate::map_builder::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 
     pub use bracket_lib::prelude::*;
     pub use legion::systems::CommandBuffer;
@@ -29,7 +31,9 @@ use prelude::*;
 struct State {
     ecs: World,
     resources: Resources,
-    systems: Schedule,
+    input_systems: Schedule,
+    player_systems: Schedule,
+    monster_systems: Schedule,
 }
 
 impl State {
@@ -50,11 +54,14 @@ impl State {
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
 
         Self {
             ecs,
             resources,
-            systems: build_scheduler(),
+            input_systems: build_input_scheduler(),
+            player_systems: build_player_scheduler(),
+            monster_systems: build_monster_scheduler(),
         }
     }
 }
@@ -67,7 +74,24 @@ impl GameState for State {
         ctx.cls();
 
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+
+        let current_state = self.resources.get::<TurnState>().unwrap().clone();
+
+        match current_state {
+            TurnState::AwaitingInput => {
+                self.input_systems
+                    .execute(&mut self.ecs, &mut self.resources);
+            }
+            TurnState::PlayerTurn => {
+                self.player_systems
+                    .execute(&mut self.ecs, &mut self.resources);
+            }
+            TurnState::MonsterTurn => {
+                self.monster_systems
+                    .execute(&mut self.ecs, &mut self.resources);
+            }
+        }
+
         render_draw_buffer(ctx).expect("Render error");
     }
 }
